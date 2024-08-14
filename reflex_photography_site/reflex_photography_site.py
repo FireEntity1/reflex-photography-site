bf5c3ed9e805bdd0714e6eb83bf220e57a696540
FireEntity1 <134467282+FireEntity1@users.noreply.github.com>
2024-08-13 18:29:46 -0600

    More photos + Glow

--- reflex_photography_site/reflex_photography_site.py
+++ reflex_photography_site/reflex_photography_site.py
@@ -16,7 +16,8 @@ def index() -> rx.Component:
     return rx.container(
         rx.color_mode.button(position="top-right"),
         rx.vstack(
-            rx.heading("FireEntity's Aviation Photography", size="9"),rx.divider(width="100%",margin_bottom="5%",margin_top="10%"),
+            rx.heading("FireEntity's Aviation Photography", size="9"),
+            rx.divider(width="100%",margin_bottom="5%",margin_top="5%"),
             rx.text(
                 "A collection of photos I've taken over the months!",
                 size="5",margin_bottom="8%",
@@ -38,8 +39,10 @@ def index() -> rx.Component:
                     rx.center(
                     rx.code("KLM 777-200",size="6"),
                     width="100%",),
-                ),
-                initial={"scale": 1},
+                style={
+        "box-shadow": "0 0 15px 5px rgba(0, 123, 255, 0.4)",
+    },),
+                initial={"scale": 0.9},
                 while_hover={"scale": 1.1, "rotate": 5,"translate": {50,50,50}},
                 while_tap={"scale": 1.3},
                 transition={"type": "spring", "stiffness": 500, "damping": 25,},
@@ -60,16 +63,252 @@ def index() -> rx.Component:
                     ),
                     rx.center(
                     rx.code("Air Canada Express Dash-8",size="6"),
-                    width="100%",),
+                    width="100%",),style={
+        "box-shadow": "0 0 15px 5px rgba(255, 50, 0, 0.5)",
+    },
                 size="4",flex_grow="5"
                 ),
-                initial={"scale": 1},
+                initial={"scale": 0.9},
                 while_hover={"scale": 1.1, "rotate": 5,"translate": {50,50,50}},
                 while_tap={"scale": 1.3},
                 transition={"type": "spring", "stiffness": 500, "damping": 25,},
             ),href="/Aug11-02.jpg",is_external=True),margin_bottom="5%",
             ),
             
+            rx.center(rx.link(
+                motion(
+                rx.card(
+                    rx.inset(
+                        rx.image(
+                            src="/Aug11-04.jpg",
+                            width="90vw",
+                            height="auto",
+                        ),
+                        side="top",
+                        pb="current",
+                    ),
+                    rx.center(
+                    rx.code("Air Canada 737 Max 8",size="6"),
+                    width="100%",),style={
+        "box-shadow": "0 0 15px 5px rgba(255, 50, 0, 0.5)",
+    },
+                size="4",flex_grow="5"
+                ),
+                initial={"scale": 0.9},
+                while_hover={"scale": 1.1, "rotate": 5,"translate": {50,50,50}},
+                while_tap={"scale": 1.3},
+                transition={"type": "spring", "stiffness": 500, "damping": 25,},
+            ),href="/Aug11-04.jpg",is_external=True),margin_bottom="5%",
+            ),
+
+        rx.center(rx.link(
+                motion(
+                rx.card(
+                    rx.inset(
+                        rx.image(
+                            src="/Aug11-03.jpg",
+                            width="90vw",
+                            height="auto",
+                        ),
+                        side="top",
+                        pb="current",
+                    ),
+                    rx.center(
+                    rx.code("WestJet 737 Max 8",size="6"),
+                    width="100%",),style={
+        "box-shadow": "0 0 15px 5px rgba(0, 150, 150, 0.5)",
+    },
+                size="4",flex_grow="5"
+                ),
+                initial={"scale": 0.9},
+                while_hover={"scale": 1.1, "rotate": 5,"translate": {50,50,50}},
+                while_tap={"scale": 1.3},
+                transition={"type": "spring", "stiffness": 500, "damping": 25,},
+            ),href="/Aug11-03.jpg",is_external=True),margin_bottom="5%",
+            ),
+
+        rx.center(rx.link(
+                motion(
+                rx.card(
+                    rx.inset(
+                        rx.image(
+                            src="/KLM-OP-2.jpg",
+                            width="90vw",
+                            height="auto",
+                        ),
+                        side="top",
+                        pb="current",
+                    ),
+                    rx.center(
+                    rx.code("KLM 777-300 Orange Pride",size="6"),
+                    width="100%",),style={
+        "box-shadow": "0 0 15px 5px rgba(250, 100, 0, 0.5)",
+    },
+                size="4",flex_grow="5"
+                ),
+                initial={"scale": 0.9},
+                while_hover={"scale": 1.1, "rotate": 5,"translate": {50,50,50}},
+                while_tap={"scale": 1.3},
+                transition={"type": "spring", "stiffness": 500, "damping": 25,},
+            ),href="/KLM-OP-2.jpg",is_external=True),margin_bottom="5%",
+            ),
+
+            rx.center(rx.link(
+                motion(
+                rx.card(
+                    rx.inset(
+                        rx.image(
+                            src="/KLM-OP-3.jpg",
+                            width="90vw",
+                            height="auto",
+                        ),
+                        side="top",
+                        pb="current",
+                    ),
+                    rx.center(
+                    rx.code("KLM 777-300 Orange Pride",size="6"),
+                    width="100%",),style={
+        "box-shadow": "0 0 15px 5px rgba(250, 100, 0, 0.5)",
+    },
+                size="4",flex_grow="5"
+                ),
+                initial={"scale": 0.9},
+                while_hover={"scale": 1.1, "rotate": 5,"translate": {50,50,50}},
+                while_tap={"scale": 1.3},
+                transition={"type": "spring", "stiffness": 500, "damping": 25,},
+            ),href="/KLM-OP-3.jpg",is_external=True),margin_bottom="5%",
+            ),
+
+        rx.center(rx.link(
+                motion(
+                rx.card(
+                    rx.inset(
+                        rx.image(
+                            src="/KLM-OP-1.jpg",
+                            width="90vw",
+                            height="auto",
+                        ),
+                        side="top",
+                        pb="current",
+                    ),
+                    rx.center(
+                    rx.code("KLM 777-300 Orange Pride",size="6"),
+                    width="100%",),style={
+        "box-shadow": "0 0 15px 5px rgba(250, 100, 0, 0.5)",
+    },
+                size="4",flex_grow="5"
+                ),
+                initial={"scale": 0.9},
+                while_hover={"scale": 1.1, "rotate": 5,"translate": {50,50,50}},
+                while_tap={"scale": 1.3},
+                transition={"type": "spring", "stiffness": 500, "damping": 25,},
+            ),href="/KLM-OP-1.jpg",is_external=True),margin_bottom="5%",
+            ),
+
+            rx.center(rx.link(
+                motion(
+                rx.card(
+                    rx.inset(
+                        rx.image(
+                            src="/Aug11-07.jpg",
+                            width="90vw",
+                            height="auto",
+                        ),
+                        side="top",
+                        pb="current",
+                    ),
+                    rx.center(
+                    rx.code("Air Canada 787-8",size="6"),
+                    width="100%",),style={
+        "box-shadow": "0 0 15px 5px rgba(250, 0, 0, 0.5)",
+    },
+                size="4",flex_grow="5"
+                ),
+                initial={"scale": 0.9},
+                while_hover={"scale": 1.1, "rotate": 5,"translate": {50,50,50}},
+                while_tap={"scale": 1.3},
+                transition={"type": "spring", "stiffness": 500, "damping": 25,},
+            ),href="/Aug11-07.jpg",is_external=True),margin_bottom="5%",
+            ),
+
+            rx.center(rx.link(
+                motion(
+                rx.card(
+                    rx.inset(
+                        rx.image(
+                            src="/Aug11-06.jpg",
+                            width="90vw",
+                            height="auto",
+                        ),
+                        side="top",
+                        pb="current",
+                    ),
+                    rx.center(
+                    rx.code("WestJet Link Saab 340b",size="6"),
+                    width="100%",),style={
+        "box-shadow": "0 0 15px 5px rgba(0, 250, 100, 0.5)",
+    },
+                size="4",flex_grow="5"
+                ),
+                initial={"scale": 0.9},
+                while_hover={"scale": 1.1, "rotate": 5,"translate": {50,50,50}},
+                while_tap={"scale": 1.3},
+                transition={"type": "spring", "stiffness": 500, "damping": 25,},
+            ),href="/Aug11-06.jpg",is_external=True),margin_bottom="5%",
+            ),
+
+            rx.center(rx.link(
+                motion(
+                rx.card(
+                    rx.inset(
+                        rx.image(
+                            src="/Aug11-09.jpg",
+                            width="90vw",
+                            height="auto",
+                        ),
+                        side="top",
+                        pb="current",
+                    ),
+                    rx.center(
+                    rx.code("Flair 737 Max 8",size="6"),
+                    width="100%",),style={
+        "box-shadow": "0 0 15px 5px rgba(0, 200, 0, 0.5)",
+    },
+                size="4",flex_grow="5"
+                ),
+                initial={"scale": 0.9},
+                while_hover={"scale": 1.1, "rotate": 5,"translate": {50,50,50}},
+                while_tap={"scale": 1.3},
+                transition={"type": "spring", "stiffness": 500, "damping": 25,},
+            ),href="/Aug11-09.jpg",is_external=True),margin_bottom="5%",
+            ),
+
+        rx.center(rx.link(
+                motion(
+                rx.card(
+                    rx.inset(
+                        rx.image(
+                            src="/Aug11-08.jpg",
+                            width="90vw",
+                            height="auto",
+                        ),
+                        side="top",
+                        pb="current",
+                    ),
+                    rx.center(
+                    rx.code("Flair 737 Max 8",size="6"),
+                    width="100%",),style={
+        "box-shadow": "0 0 15px 5px rgba(0, 200, 0, 0.5)",
+    },
+                size="4",flex_grow="5"
+                ),
+                initial={"scale": 0.9},
+                while_hover={"scale": 1.1, "rotate": 5,"translate": {50,50,50}},
+                while_tap={"scale": 1.3},
+                transition={"type": "spring", "stiffness": 500, "damping": 25,},
+            ),href="/Aug11-08.jpg",is_external=True),margin_bottom="5%",
+            ),
+
             spacing="5",
             flex_wrap="wrap",
             width="100%",
